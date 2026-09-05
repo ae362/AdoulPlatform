cfb7622ec004164f56be58f317eb906e1b878d4a
XtremeGamerX <ayoubziad2004@gmail.com>
2026-09-05 16:37:48 +0100

    Apply updated gitignore rules 2

--- frontend/public/wgssSigCaptX.js
+++
@@ -1,9 +0,0 @@
-/* ************************************************************************** */
-/* wgssSigCaptX.js (Combined for local usage in port 9000 environment)        */
-/* ************************************************************************** */
-
-// This is a bridge file to ensure the application can always access the Wacom classes.
-// In your environment, the real SDK logic is inside public/demobuttons/wgssStuSdk.js
-// We will use that file as our primary library.
-
-console.log("Wacom SigCaptX Local Bridge Initialized");
